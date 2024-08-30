2f1851cfd58f1fe6478cc11a56ad67572221991e
mitoperni <mitoperni@gmail.com>
2024-08-30 13:03:08 +0000

    cCommit inicial

--- src/front/js/pages/Cart.js
+++ src/front/js/pages/Cart.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Cart() {
   return (
-    <div>Cart</div>
+    <div>
+      <div>
+        <p>Aquí tienen que ir las card con los artículos que se han metido en el carrito</p>
+      </div>
+    </div>
   )
 }
 
