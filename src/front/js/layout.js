cedf50a3cf62188731587bbd53343687bbd352a2
Miguel Toyas Pernichi <mitoperni@gmail.com>
2024-08-30 07:59:59 +0100

    FRONT-END: Vistas creadas, Rutas creadas

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,36 +2,68 @@ import React from "react";
 import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
+// Imports para las vistas de cliente
 import { Home } from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import Private from "./pages/Private";
+import Cart from  "./pages/Cart"
+import ContactUs from "./pages/ContactUs"
+import PayingForm from "./pages/PayingForm"
+import Profile from "./pages/Profile"
+import ShopDetail from "./pages/ShopDetail"
+import Shops from "./pages/Shops"
+import ShopsSearch from "./pages/ShopsSearch"
+
+// Vistas para Admins
+import AdminHome from "./pages/Admins/AdminHome"
+
+// Vistas para Tiendas
+import ShopHome from "./pages/Shops/ShopHome"
+import ShopSignUp from "./pages/Shops/ShopSignUp"
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
-import injectContext from "./store/appContext";  // Asegúrate de importar correctamente
+import injectContext from "./store/appContext"; // Asegúrate de importar correctamente
 
 const Layout = () => {
-    const basename = process.env.BASENAME || "";
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<SignUp />} path="/signup" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<Private />} path="/private" />
-                        <Route element={<h1>Not found!</h1>} path="*" />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+
+            {/* Vistas de cliente */}
+            <Route element={<Home />} path="/" />
+            <Route element={<SignUp />} path="/signup" />
+            <Route element={<Login />} path="/login" />
+            <Route element={<Private />} path="/private" />
+            <Route element={<Cart />} path="/cart" />
+            <Route element={<ContactUs />} path="/contactus" />
+            <Route element={<PayingForm />} path="/payingform" />
+            <Route element={<Profile />} path="/profile" />
+            <Route element={<ShopDetail />} path="/shopdetail/:id" />
+            <Route element={<Shops />} path="/shops" />
+            <Route element={<ShopsSearch />} path="/shopssearch" />
+
+            {/* Vistas de Admin */}
+            <Route element={<AdminHome />} path="/adminhome" />
+
+            {/* Vistas de Tienda */}
+            <Route element={<ShopHome />} path="/shophome" />
+            <Route element={<ShopSignUp />} path="/shopsignup" />
+
+            <Route element={<h1>Not found!</h1>} path="*" />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 // Aplica injectContext al Layout
