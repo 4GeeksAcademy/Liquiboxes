063637d0f7c4d8d7070c40a468648056ab5ba9ed
Miguel Toyas Pernichi <mitoperni@gmail.com>
2024-08-30 07:26:19 +0100

    Front: Creando Views

---
+++ src/front/js/pages/Shops/ShopSignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ShopSignUp() {
+  return (
+    <div>
+      <form>
+        <input></input>
+      </form>
+    </div>
+  );
+}
+
+export default ShopSignUp;
