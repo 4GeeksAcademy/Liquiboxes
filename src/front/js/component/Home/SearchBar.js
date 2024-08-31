03dafd7ba507196e13b9fadc3fabe43dae9d4ef3
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-08-31 02:55:56 +0000

    create ScrollHorizontal component and map in home

--- src/front/js/component/Home/SearchBar.js
+++ src/front/js/component/Home/SearchBar.js
@@ -2,13 +2,19 @@ import React from "react";
 
 export default function SearchBar() {
     return (
-        <div>
-            <div className="input-group mb-3">
-                <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
-                    <div className="input-group-append">
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-                    </div>
-            </div>
-        </div>
+        <form className="d-flex align-items-center p-2" role="search">
+            <input
+                className="form-control border-0 border-bottom shadow"            
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+            />
+            <button className="btn btn-primary ms-2 px-4" type="submit">Search</button>
+        </form>
+
+
     )
 }
+
+
+
